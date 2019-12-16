552f50dfdc12a62d458b2d6b56ff082200954b5e
fixnixinc <devops@fixnix.co>
2019-12-16 14:39:47 +0530

    Update constants.php

--- php/common/constants.php
+++ php/common/constants.php
@@ -2,6 +2,6 @@
     // Constants for connecting to DB
     define ('SERVER_NAME', 'localhost');
     define ('DB_USER_NAME', 'root');
-    define ('DB_PASSWORD', '299ee42d5d3a6a5f8233bf5787645f14Fixnix2019!@');
+    define ('DB_PASSWORD', 'password');
     define ('DB_SCHEMA', 'freshgrc');
     define ('CONF_KEY_ROUTING', 'routing');
