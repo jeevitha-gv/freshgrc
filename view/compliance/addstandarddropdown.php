266472f3c0b5fc0b82749ed19c11829ad93129c5
ROBIN-5894 <robin@fixnix.co>
2019-12-20 18:27:23 +0530

    add_btn

--- view/compliance/addstandarddropdown.php
+++ view/compliance/addstandarddropdown.php
@@ -14,7 +14,7 @@
       <option value="<?php echo $compliance['id'] ?>"><?php echo $compliance['name'] ?></option>
     <?php } ?>
   </select>
-  <button class="btn btn-outline-secondary" onclick="addstand(), location.reloas(true)" title="Add Checklists" style="cursor: pointer;height: 40px; border-radius: 25px; margin-left: 5px;"><span class="la la-plus-circle"></span></button>
+  <button class="btn btn-outline-secondary" onclick=" addstand(), location.reload(true) " title="Add Checklists" style="cursor: pointer;height: 40px; border-radius: 25px; margin-left: 5px;"><span class="la la-plus-circle"></span></button>
   <a href="view/common/regulatoryTemplate.php" style="margin-left: 22%;"><img src="import.png" title="Import CheckLists" width="45" height="40"></a>
 </div>
 </div>
