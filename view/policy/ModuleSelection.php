ea715e5a5bc0430312b836ea2cfba3675f9c4a14
sivaraman1995 <sivaraman@fixnix.co>
2019-12-21 14:53:24 +0530

    header

--- view/policy/ModuleSelection.php
+++ view/policy/ModuleSelection.php
@@ -86,7 +86,7 @@ font-size:12px;
  
 </style>
 <?php
-    include '../siteHeader.php';
+    include '../siteHeaderforsetupguide.php';
     ?>
 <body class="kt-quick-panel--right kt-demo-panel--right kt-offcanvas-panel--right kt-header--fixed kt-header-mobile--fixed kt-subheader--fixed kt-subheader--enabled kt-subheader--solid kt-aside--enabled kt-aside--fixed kt-page--loading" style="background-color: #EEEEEE;">
 <!-- <div class="conatiner">
