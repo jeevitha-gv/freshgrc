427ff4bb68bbb14e886b13c6975aad1a62d5898f
Manish Kumar <manish@fixnix.co>
2019-12-24 14:38:14 +0530

    Sendgrid Mailer

--- php/common/vendor/composer/autoload_real.php
+++ php/common/vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit3fd76fcc7704778841111a412792a346
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit3fd76fcc7704778841111a412792a346::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire3fd76fcc7704778841111a412792a346($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire3fd76fcc7704778841111a412792a346($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
