ad328d212053fec17c2104b2053ff74899940e73
krishna-Eniyavan <krisha@fixnix.co>
2020-03-05 18:22:21 +0530

    login page

--- login.php
+++ login.php
@@ -115,9 +115,19 @@ function lsRememberMe() {
   }
 }
 </script>
-
 <!DOCTYPE html>
-
+<!--
+Template Name: Metronic - Responsive Admin Dashboard Template build with Twitter Bootstrap 4 & Angular 8
+Author: KeenThemes
+Website: http://www.keenthemes.com/
+Contact: support@keenthemes.com
+Follow: www.twitter.com/keenthemes
+Dribbble: www.dribbble.com/keenthemes
+Like: www.facebook.com/keenthemes
+Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
+Renew Support: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
+License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
+-->
 <html lang="en" >
     <!-- begin::Head -->
     <head><!--begin::Base Path (base relative path for assets of this page) -->
@@ -133,7 +143,7 @@ function lsRememberMe() {
 
         
                     <!--begin::Page Custom Styles(used by this page) -->
-                             <link href="./assets/css/demo3/pages/login/login-6.css" rel="stylesheet" type="text/css" />
+                             <link href="./assets/css/demo3/pages/login/login-2.css" rel="stylesheet" type="text/css" />
                         <!--end::Page Custom Styles -->
         
         <!--begin:: Global Mandatory Vendors -->
@@ -169,9 +179,15 @@ function lsRememberMe() {
 <link href="./assets/vendors/custom/vendors/flaticon/flaticon.css" rel="stylesheet" type="text/css" />
 <link href="./assets/vendors/custom/vendors/flaticon2/flaticon.css" rel="stylesheet" type="text/css" />
 <link href="./assets/vendors/general/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css" />
+<!--end:: Global Optional Vendors -->
 
+<!--begin::Global Theme Styles(used by all pages) -->
+                    
                     <link href="./assets/css/demo3/style.bundle.css" rel="stylesheet" type="text/css" />
-       
+                <!--end::Global Theme Styles -->
+
+        <!--begin::Layout Skins(used by all pages) -->
+                <!--end::Layout Skins -->
 
         <link rel="shortcut icon" href="./assets/media/logos/fixnix.png" />
     </head>
@@ -181,73 +197,104 @@ function lsRememberMe() {
     <body  class="kt-quick-panel--right kt-demo-panel--right kt-offcanvas-panel--right kt-header--fixed kt-header-mobile--fixed kt-subheader--fixed kt-subheader--enabled kt-subheader--solid kt-aside--enabled kt-aside--fixed kt-page--loading"  >
 
        
-    	<!-- begin:: Page -->
-	<div class="kt-grid kt-grid--ver kt-grid--root kt-page">
-		<div class="kt-grid kt-grid--hor kt-grid--root  kt-login kt-login--v6 kt-login--signin" id="kt_login">
-    <div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--desktop kt-grid--ver-desktop kt-grid--hor-tablet-and-mobile">
-        <div class="kt-grid__item  kt-grid__item--order-tablet-and-mobile-2  kt-grid kt-grid--hor kt-login__aside">
-            <div class="kt-login__wrapper">
-                <div class="kt-login__container">
-                    <div class="kt-login__body">
-                        <div class="kt-login__logo">
-                            <a href="#">
-                                <img src="./assets/media/company-logos/fixnix.png" style="height: 100px;width: 100px;">      
-                            </a>
-                        </div>
-
-                        <div class="kt-login__signin">
-                            <div class="kt-login__head">
-                                <h3 class="kt-login__title">Sign In To Admin</h3>
-                            </div>
-                            <div class="kt-login__form">
-                                <form class="kt-form" name="login" method="post" name="loginform">
-                                    <div class="form-group">
-                                        <input class="form-control" type="text" placeholder="Email" id="email" name="email" autocomplete="off">
-                                    </div>
-                                    <div class="form-group">
-                                        <input class="form-control form-control-last" type="password" id="pass" placeholder="Password" name="password">
-                                    </div>
-                                 <!--    <div class="kt-login__extra">
-                                        <label class="kt-checkbox">
-                                          <input type="checkbox" value="lsRememberMe" id="rememberMe"> <label for="rememberMe">Remember me</label>
-                                            <span></span>
-                                        </label>                        
-                                        <a href="javascript:;" id="kt_login_forgot">Forget Password ?</a>
-                                    </div> -->
-                                    <div class="kt-login__actions">
-                                        <button  type="submit" name="login" onclick="lsRememberMe()" class="btn btn-brand btn-pill btn-elevate">Sign In</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-
-                        
-
-                        
-                    </div>
-                </div>
-                <div class="kt-login__account">
-                    <span class="kt-login__account-msg">
-                        Don't have an account yet ?
-                    </span>&nbsp;
-                    <a href="signup.php">Sign Up</a>
-                </div>
-            </div>
+      <!-- begin:: Page -->
+  <div class="kt-grid kt-grid--ver kt-grid--root kt-page">
+    <div class="kt-grid kt-grid--hor kt-grid--root kt-login kt-login--v2 kt-login--signin" id="kt_login">
+  <div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor" style="background-image: url(./assets/media//bg/bg-1.jpg);">
+    <div class="kt-grid__item kt-grid__item--fluid kt-login__wrapper">
+      <div class="kt-login__container">
+        <div class="kt-login__logo">
+            <a>
+                <img src="./assets/media/company-logos/fixnix.png" style="height: 130px;width: 150px;">      
+            </a>
         </div>
-
-        <div class="kt-grid__item kt-grid__item--fluid kt-grid__item--center kt-grid kt-grid--ver kt-login__content" style="background-image: url(./assets/media//bg/data.jpg);">
-            <div class="kt-login__section">
-                <div class="kt-login__block">
-                    <h3 class="kt-login__title">Join Our Community</h3>
-                    
-                </div>
+        <div class="kt-login__signin">
+          <div class="kt-login__head">
+            <h3 class="kt-login__title">Sign In To Admin</h3>
+          </div>
+         <form class="kt-form" name="login" method="post" name="loginform">
+             <div class="form-group">
+                 <input class="form-control" type="text" style="color: white;" placeholder="Email" id="email" name="email" autocomplete="off">
+             </div>
+             <div class="form-group">
+                 <input class="form-control form-control-last" style="color: white;" type="password" id="pass" placeholder="Password" name="password">
+             </div>
+          <!--    <div class="kt-login__extra">
+                 <label class="kt-checkbox">
+                   <input type="checkbox" value="lsRememberMe" id="rememberMe"> <label for="rememberMe">Remember me</label>
+                     <span></span>
+                 </label>                        
+                 <a href="javascript:;" id="kt_login_forgot">Forget Password ?</a>
+             </div> -->
+             <div class="kt-login__actions">
+              <button  type="submit" name="login" onclick="lsRememberMe()" class="btn btn-pill kt-login__btn-primary">Sign In</button>
             </div>
+             <!-- <div class="kt-login__actions">
+                 <button  type="submit" name="login" onclick="lsRememberMe()" class="btn btn-brand btn-pill btn-elevate">Sign In</button>
+             </div> -->
+         </form>
         </div>
+        <div class="kt-login__signup">
+          <div class="kt-login__head">
+            <h3 class="kt-login__title">Sign Up</h3>
+            <div class="kt-login__desc">Enter your details to create your account:</div>
+          </div>
+          <form class="kt-login__form kt-form" action="">
+            <div class="input-group">
+              <input class="form-control" type="text"  placeholder="Fullname" name="fullname">
+            </div>
+            <div class="input-group">
+              <input class="form-control" type="text" placeholder="Email" name="email" autocomplete="off">
+            </div>
+            <div class="input-group">
+              <input class="form-control" type="password" placeholder="Password" name="password">
+            </div>
+            <div class="input-group">
+              <input class="form-control" type="password" placeholder="Confirm Password" name="rpassword">
+            </div>
+            <div class="row kt-login__extra">
+              <div class="col kt-align-left">
+                <label class="kt-checkbox">
+                  <input type="checkbox" name="agree">I Agree the <a href="#" class="kt-link kt-login__link kt-font-bold">terms and conditions</a>.
+                  <span></span>
+                </label>
+                <span class="form-text text-muted"></span>
+              </div>
+            </div>
+            <div class="kt-login__actions">
+              <button id="kt_login_signup_submit" class="btn btn-pill kt-login__btn-primary">Sign Up</button>&nbsp;&nbsp;
+              <button id="kt_login_signup_cancel" class="btn btn-pill kt-login__btn-secondary">Cancel</button>
+            </div>
+          </form>
+        </div>
+        <div class="kt-login__forgot">
+          <div class="kt-login__head">
+            <h3 class="kt-login__title">Forgotten Password ?</h3>
+            <div class="kt-login__desc">Enter your email to reset your password:</div>
+          </div>
+          <form class="kt-form" action="">
+            <div class="input-group">
+              <input class="form-control" type="text" placeholder="Email" name="email" id="kt_email" autocomplete="off">
+            </div>
+            <div class="kt-login__actions">
+              <button id="kt_login_forgot_submit" class="btn btn-pill kt-login__btn-primary">Request</button>&nbsp;&nbsp;
+              <button id="kt_login_forgot_cancel" class="btn btn-pill kt-login__btn-secondary">Cancel</button>
+            </div>
+          </form>
+        </div>
+        
+        <div class="kt-login__account">
+          <span class="kt-login__account-msg">
+            Don't have an account yet ?
+          </span>&nbsp;&nbsp;
+          <a href="./signup.php"  class="kt-link kt-link--light kt-login__account-link">Sign Up</a>
+        </div>
+      </div>  
     </div>
-</div>         	</div>
-	
-
-
+  </div>
+</div>  
+  </div>
+  
 <!-- end:: Page -->
 
 
@@ -257,7 +304,7 @@ function lsRememberMe() {
         </script>
         <!-- end::Global Config -->
 
-    	<!--begin:: Global Mandatory Vendors -->
+      <!--begin:: Global Mandatory Vendors -->
 <script src="./assets/vendors/general/jquery/dist/jquery.js" type="text/javascript"></script>
 <script src="./assets/vendors/general/popper.js/dist/umd/popper.js" type="text/javascript"></script>
 <script src="./assets/vendors/general/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
@@ -328,9 +375,9 @@ function lsRememberMe() {
 <!--end:: Global Optional Vendors -->
 
 <!--begin::Global Theme Bundle(used by all pages) -->
-    	    	   
-		    	   <script src="./assets/js/demo3/scripts.bundle.js" type="text/javascript"></script>
-				<!--end::Global Theme Bundle -->
+               
+             <script src="./assets/js/demo3/scripts.bundle.js" type="text/javascript"></script>
+        <!--end::Global Theme Bundle -->
 
         
                     <!--begin::Page Scripts(used by this page) -->
